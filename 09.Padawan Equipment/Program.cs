c8eb1e6ffb9c28a4a4e583ad087dabc635f6f8f4
Hristiyan Velev <145477797+hristiyan96@users.noreply.github.com>
2024-02-28 17:24:06 +0200

    Add files via upload

---
+++ 09.Padawan Equipment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float money = float.Parse(Console.ReadLine());
+        int numOfStudents = int.Parse(Console.ReadLine());
+        double lightsaber = double.Parse(Console.ReadLine());
+        double robe = double.Parse(Console.ReadLine());
+        double belt = double.Parse(Console.ReadLine());
+        double neededEquipment = 0;
+        double freeBelts = numOfStudents / 6;
+        double students1 = Math.Ceiling((numOfStudents * 0.1) + numOfStudents);
+        neededEquipment = (students1 * lightsaber) + (numOfStudents * robe) + ((numOfStudents-freeBelts) * belt);
+
+        if(neededEquipment > money)
+        {
+            Console.WriteLine($"John will need {neededEquipment - money:f2}lv more.");
+        }
+        else if(neededEquipment <= money)
+        {
+            Console.WriteLine($"The money is enough - it would cost {neededEquipment:f2}lv.");
+        }
+        
+
+
+
+
+    }
+}
---
+++ 10.Rage Expenses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int lostGames = int.Parse(Console.ReadLine());
+        decimal headset = decimal.Parse(Console.ReadLine());
+        decimal mouse = decimal.Parse(Console.ReadLine());
+        decimal keyboard = decimal.Parse(Console.ReadLine());
+        decimal display = decimal.Parse(Console.ReadLine());
+
+        decimal trashedHeadset = (lostGames/2) * headset;
+        decimal trashedMouse = (lostGames/3) * mouse;
+        decimal trashedKeyboard = (lostGames/6) * keyboard;
+        decimal trashedDisplay = (lostGames/12) * display;
+        
+        decimal expenses = trashedHeadset + trashedMouse + trashedKeyboard + trashedDisplay;
+        Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+            
+    }
+}
